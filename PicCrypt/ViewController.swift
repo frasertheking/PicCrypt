310a41e6345c8c85492e4daa414792c273f49114
Fraser King <frasertheking@gmail.com>
2016-12-10 11:45:29 -0500

    Initial Commit

---
+++ PicCrypt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PicCrypt
+//
+//  Created by Fraser King on 2016-12-10.
+//  Copyright © 2016 Fraser King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
